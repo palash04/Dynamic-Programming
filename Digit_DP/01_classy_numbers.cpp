a3b3ffe24275ba57f54e038ee30528de41c23438
palash04 <palashkamble22@gmail.com>
2021-07-19 13:13:06 +0530

    Create 01_classy_numbers.cpp

---
+++ Digit_DP/01_classy_numbers.cpp
@@ -0,0 +1,68 @@
+/*
+
+https://codeforces.com/problemset/problem/1036/C
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* macros */
+#define inf 1e9
+#define int long long
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vt vector
+#define pi pair<int,int>
+#define tc int t;cin>>t;while (t--){solve();}
+#define gtc int t;cin>>t;int TC=1;while (t--) {cout << "Case #" << TC << ": " << solve() << "\n";TC++;}
+#define gvtc int t;cin>>t;int TC=1;while (t--) {cout << "Case #" << TC << ": "; vsolve(); TC++;}
+#define notc solve();
+#define mod 1000000007
+#define ret return 0;
+
+string s;
+int dp[20][4][2];
+
+int go (int pos, int cnt, int tight) {
+    if (pos == s.size()) {
+        return 1;
+    }
+    if (dp[pos][cnt][tight] != -1) {
+        return dp[pos][cnt][tight];
+    }
+    int en = tight ? s[pos] - '0' : 9;
+
+    int ans = 0;
+
+    for (int i=0;i<=en;i++) {
+        if (cnt + (i>0) <= 3) {
+            ans += go (pos + 1, cnt + (i>0), tight & (i == en));
+        }
+    }
+    return dp[pos][cnt][tight] = ans;
+}
+
+void solve() {
+    int l,r;
+    cin>>l>>r;
+
+    int ans = 0;
+    s = to_string(r);
+    memset(dp, -1, sizeof dp);
+    ans += go (0, 0, 1);
+    l -= 1;
+    s = to_string(l);
+    memset(dp, -1, sizeof dp);
+    ans -= go (0, 0, 1);
+    cout << ans << "\n";
+}
+
+int32_t main(){
+    fastio
+    tc
+    ret
+}
